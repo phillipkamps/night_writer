85f86f406634ff2caeb40e1de077d869eb4ae833
Phillip Kamps <phillip.kamps@gmail.com>
2022-03-06 17:39:19 -0700

    Split dictionary from Writer to module
    - Test: Reader exists, has dictionary
    - Feat: Reader, #braille_to_eng_dict

---
+++ lib/reader.rb
@@ -0,0 +1,12 @@
+require "./lib/dictionary"
+
+class Reader
+  include Dictionary
+
+  attr_reader :braille_to_eng_dict
+
+  def initialize
+    @braille_to_eng_dict = eng_to_braille_dict.invert
+    binding.pry
+  end
+end
