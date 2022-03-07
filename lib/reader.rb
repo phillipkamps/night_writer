7ae1e8353c85c18af2592386243084ea828dd501
Phillip Kamps <phillip.kamps@gmail.com>
2022-03-06 18:21:03 -0700

    Feat: #translate (single character)

--- lib/reader.rb
+++ lib/reader.rb
@@ -3,13 +3,15 @@ require "./lib/dictionary"
 class Reader
   include Dictionary
 
-  attr_reader :braille_to_eng_dict
+  attr_reader :braille_to_eng_dict, :message
 
   def initialize(message)
+    @message = message
     @braille_to_eng_dict = eng_to_braille_dict.invert
   end
 
   def translate
-    "a"
+    message_array = message.split("\n")
+    braille_to_eng_dict[message_array]
   end
 end
